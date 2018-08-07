711a7a2b1f20de78a9c50e161ea2421baaa0577a
Sengkryvy <sengkryvy@gmail.com>
2018-08-08 00:16:43 +0700

    Add horror activity

--- app/src/main/java/gictitc/storygame/storygame/MainActivity.java
+++ app/src/main/java/gictitc/storygame/storygame/MainActivity.java
@@ -1,13 +1,27 @@
 package gictitc.storygame.storygame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import gictitc.storygame.storygame.horror_activity.HorrorStart;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHorror;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Horror(View view) {
+        btnHorror = (Button)view;
+        Intent i = new Intent(getApplicationContext(), HorrorStart.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/gictitc/storygame/storygame/horror_activity/HorrorStart.java
@@ -0,0 +1,27 @@
+package gictitc.storygame.storygame.horror_activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import gictitc.storygame.storygame.MainActivity;
+import gictitc.storygame.storygame.R;
+
+public class HorrorStart extends AppCompatActivity {
+
+    Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horror_start);
+    }
+
+    public void Next(View view) {
+        btnNext = (Button)view;
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+}
