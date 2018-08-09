3627d1803b0ba939c511db91f358321a44b8e251
Sengkryvy <sengkryvy@gmail.com>
2018-08-09 12:09:05 +0700

    add the start of horror story

--- app/src/main/java/gictitc/storygame/storygame/horror_activity/HorrorStart.java
+++ app/src/main/java/gictitc/storygame/storygame/horror_activity/HorrorStart.java
@@ -1,6 +1,8 @@
 package gictitc.storygame.storygame.horror_activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,28 @@ public class HorrorStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horror_start);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure leaving the current game?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void Next(View view) {
